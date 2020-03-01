7384c173377ff59bc6926790f9c4e1f3fa14da1e
Akram <900658008.akram@gmail.com>
2020-03-01 15:24:00 +0500

    fixed: tests

--- pkg/rcp/constans.go
+++ pkg/rcp/constans.go
@@ -9,9 +9,9 @@ const Tcp = "tcp"
 const Endl = "\n"
 const AddressClient = "localhost:9999"
 
-const PathFileServer = "./cmd/rcp/server/files/"
-const PathFileClient = "./cmd/rcp/client/files/"
-const PathTestData = "./cmd/rcp/server/testdata/"
+const PathFileServer = "cmd/rcp/server/files/"
+const PathFileClient = "cmd/rcp/client/files/"
+const PathTestData = "cmd/rcp/server/testdata/"
 
 
 const TimeSleep = 1_000_000_000
